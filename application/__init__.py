582ac014508640cbb2bd9094ff7513fbdf431ef9
Frank Hsieh <43116359+kelhwh@users.noreply.github.com>
2020-12-30 21:30:33 +0000

    Organising structure (#1)
    * create folder structure
    
    * structure core folder
    
    * build app.py and application __init__
    
    * create gitignore
    
    * app.py typo
    
    * build base templates and welcome templates for core blueprint

---
+++ application/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret'
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(base_dir, 'database.sqlite')
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view('users.login')
+
+
+from application.core.views import core
+
+app.register_blueprint(core)
