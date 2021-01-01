f7caa1431a2104519268d2304b19c2914c93d5ff
Frank Hsieh <frank1571@gmail.com>
2021-01-01 17:11:33 +0000

    update devresource

--- devresources/get_fhir_patient.py
+++ devresources/get_fhir_patient.py
@@ -1,4 +1,5 @@
 from fhirclient import client
+from fhirclient.models.fhirsearch import FHIRSearch
 
 settings = {
     'app_id': 'my_web_app',
@@ -7,18 +8,29 @@ settings = {
 
 smart = client.FHIRClient(settings=settings)
 
-import fhirclient.models.patient as p
-patient = p.Patient.read('identifier=http://hl7.org/fhir/sid/us-ssn|999-45-3776', smart.server)
+from fhirclient.models import patient as p
+
+struct = {'identifier':'http://hl7.org/fhir/sid/us-ssn|999-45-3776'}
+#patient = p.Patient.read('1349276', smart.server)
+search = p.Patient.where(struct=struct)
+patient = search.perform(smart.server).entry[0].resource
+patient = search.perform_resources(smart.server)[0]
 
 print(patient.birthDate.isostring)
 
+print(patient.id)
+
 print(smart.human_name(patient.name[0]))
+print(search.resource_type.resource_type)
 
 
-import fhirclient.models.encounter as encounter
-#search = p.Procedure.where(struct={'subject': 'hca-pat-1', 'status': 'completed'})
-search = encounter.Encounter.where(struct={'subject':'1349276'})
-encounters = search.perform_resources(smart.server)
+# import fhirclient.models.encounter as encounter
+# #search = p.Procedure.where(struct={'subject': 'hca-pat-1', 'status': 'completed'})
+# search = encounter.Encounter.where(struct={'subject':'1349276'})
+# """perform gives back bundle, perform_resources gives back a list of resources"""
+# print(search.perform(smart.server).link[0].relation)
+# encounters = search.perform_resources(smart.server)
+# print(len(encounters))
 #
 # for e in encounters:
 #     print(e.as_json())
