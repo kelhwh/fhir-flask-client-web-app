779733f9dcd05553bed8b8eadeeb3686178c61bf
Frank Hsieh <frank1571@gmail.com>
2021-01-01 17:14:55 +0000

    create check_duplicate validator, add identifier fields

--- application/users/forms.py
+++ application/users/forms.py
@@ -1,12 +1,27 @@
 from flask import flash
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField, PasswordField
+from wtforms.fields import StringField, PasswordField, SubmitField, PasswordField, SelectField
 from wtforms.fields.html5 import EmailField, DateField
 from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length
 from wtforms import ValidationError
 
 from flask_login import current_user
 from application.models import UserModel
+
+class check_duplicate():
+    def __init__(self, model, field, message=None):
+        self.model = model
+        self.field = field
+
+        if not message:
+            message = f'The {self.field.name} has been registerd already.'
+        self.message = message
+
+    def __call__(self, form, field):
+        instance = self.model.query.filter(self.field == field.data).first()
+        if instance:
+            raise ValidationError(self.message)
+
 
 
 class LoginForm(FlaskForm):
@@ -27,15 +42,8 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
-    first_name = StringField(
-        'First Name',
-        validators=[
-            InputRequired(),
-            Length(max=64, message='Must be less than %(max)d characters long.')
-        ]
-    )
-    last_name = StringField(
-        'Last Name',
+    family_name = StringField(
+        'Family Name',
         validators=[
             InputRequired(),
             Length(max=64, message='Must be less than %(max)d characters long.')
@@ -46,19 +54,30 @@ class RegistrationForm(FlaskForm):
         format='%Y-%m-%d',
         validators=[InputRequired()]
     )
-    insurance_id = StringField(
-        'Insurance ID Number',
+    identifier_system = SelectField(
+        'ID',
+        choices=[
+            ('http://hl7.org/fhir/sid/us-ssn', 'Social Security Number')
+        ],
+        validators=[
+            InputRequired()
+        ]
+    )
+    identifier_value = StringField(
+        'ID Number',
         validators=[
             InputRequired(),
             Length(max=64, message='Must be less than %(max)d characters long.')
-        ]
+        ],
+        render_kw = {"placeholder": "Your ID Number"}
     )
     email = EmailField(
         'Email',
         validators=[
             InputRequired(),
             Email('Please provide a valid email address'),
-            Length(max=64, message='Must be less than %(max)d characters long.')
+            Length(max=64, message='Must be less than %(max)d characters long.'),
+            check_duplicate(model=UserModel, field=UserModel.email)
         ]
     )
     password = PasswordField(
