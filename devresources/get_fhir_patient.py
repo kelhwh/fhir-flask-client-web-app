967f4723bd2a46f5b35c5884b8f42513c9b2981c
Frank Hsieh <frank1571@gmail.com>
2021-01-02 00:18:36 +0000

    update devresource with encounter script

--- devresources/get_fhir_patient.py
+++ devresources/get_fhir_patient.py
@@ -1,5 +1,6 @@
 from fhirclient import client
 from fhirclient.models.fhirsearch import FHIRSearch
+from fhirclient.models.encounter import Encounter
 
 settings = {
     'app_id': 'my_web_app',
@@ -23,14 +24,14 @@ print(patient.id)
 print(smart.human_name(patient.name[0]))
 print(search.resource_type.resource_type)
 
+search = Encounter.where(struct={'patient':patient.id, '_count':'10000'})
+encounter = search.perform_resources(smart.server)
+print(len(encounter))
 
-# import fhirclient.models.encounter as encounter
-# #search = p.Procedure.where(struct={'subject': 'hca-pat-1', 'status': 'completed'})
-# search = encounter.Encounter.where(struct={'subject':'1349276'})
-# """perform gives back bundle, perform_resources gives back a list of resources"""
-# print(search.perform(smart.server).link[0].relation)
-# encounters = search.perform_resources(smart.server)
-# print(len(encounters))
-#
-# for e in encounters:
-#     print(e.as_json())
+"""perform gives back bundle, perform_resources gives back a list of resources"""
+search = Encounter.where(struct={'patient':patient.id, '_count':'10000'})
+encounters = search.perform_resources(smart.server)
+#print(encounters.as_json())
+
+for e in encounters:
+    print(e.type[0].text)
