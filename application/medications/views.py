7cb0d6370aaac8006a59b7a23e404e066e21b2e3
Frank Hsieh <frank1571@gmail.com>
2021-01-17 23:53:06 +0000

    fetch pratitioner and condition resource in medication page

--- application/medications/views.py
+++ application/medications/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, Blueprint, redirect, url_for
 from flask_login import current_user, login_required
+from fhirclient.models import condition, practitioner
 from application.fhir.connect import smart
 from application.fhir.search import ResourceFinder
 from datetime import datetime
@@ -23,10 +24,23 @@ def timeline():
 
         dict = {}
         dict['medication_name'] = m.get('medicationCodeableConcept', {}).get('text')
-        dict['pratitioner'] = m.get('requester', {}).get('agent')
-        dict['reason'] = m.get('reasonReference')[0].get('reference') if m.get('reasonReference') else 'Not provided'
+        #dict['pratitioner_name'] = m.get('requester', {}).get('agent').get('reference')
+        #dict['reason'] = m.get('reasonReference', [{}])[0].get('reference')
         dict['date'] = datetime.fromisoformat(m.get('authoredOn')).date().__str__()
+        if m.get('requester', {}).get('agent', {}).get('reference') is not None:
+            resource = practitioner.Practitioner.read_from(m.get('requester', {}).get('agent').get('reference'), smart.server)
+            dict['practitioner_name'] = smart.human_name(resource.name[0])
+
+        if m.get('reasonReference', [{}])[0].get('reference') is not None:
+            resource = condition.Condition.read_from(m.get('reasonReference', [{}])[0].get('reference'), smart.server)
+            dict['reason'] = resource.code.text
 
         medications.append(dict)
+        medications = [i for i in sorted(medications, key=lambda item: item['date'], reverse=True)]
 
     return render_template('timeline.html', medications=medications)
+
+
+@medications_bp.route('/grid')
+def grid():
+    return render_template('grid.html')
