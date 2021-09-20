a66bca632a590d4fe968f1814115bf59c21134d2
kelhwh <frank1571@gmail.com>
2021-09-20 22:46:44 +0800

    change target fhir server to epic sandbox

--- application/fhir/connect.py
+++ application/fhir/connect.py
@@ -2,8 +2,9 @@ from fhirclient import client
 from fhirclient.models.fhirsearch import FHIRSearch
 
 settings = {
-    'app_id': 'my_web_app',
-    'api_base':  'http://hapi.fhir.org/baseDstu3/'
+    'app_id': '8ba7dd62-013b-4c3f-94b2-2f10004a8dde',
+    'redirect_uri': 'http://127.0.0.1:5000/oauth/epic/callback',
+    'api_base': 'https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/STU3/'
 }
 
 smart = client.FHIRClient(settings=settings)
