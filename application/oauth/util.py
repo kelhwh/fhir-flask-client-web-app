89a30f3fb7dd9c1e24eacf410422bfaa8ddc20e7
kelhwh <frank1571@gmail.com>
2021-09-21 21:25:26 +0800

    create decorator oauth_required

---
+++ application/oauth/util.py
@@ -0,0 +1,17 @@
+from flask import redirect, render_template, url_for
+from fhirclient.server import FHIRUnauthorizedException
+from fhirclient.models import patient
+from functools import wraps
+from application.fhir.connect import smart
+
+def oauth_required(func):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except:
+            return redirect(url_for('oauth_bp.index'))
+        else:
+            return render_template('error.html')
+        return func(*args, **kwargs)
+    return decorated_view
