6a221bdd945bcf0e5469610371947bc4d1e4b18b
kelhwh <frank1571@gmail.com>
2021-09-21 20:33:35 +0800

    create and login user after oauth successful

--- application/oauth/views.py
+++ application/oauth/views.py
@@ -1,10 +1,11 @@
 from flask import render_template, Blueprint, redirect, url_for,flash, request
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user
 from application import db
 from application.models import UserModel
-from application.users.forms import LoginForm, RegistrationForm
 from application.fhir.search import ResourceFinder
 from application.fhir.connect import smart
+import uuid
+from datetime import date
 
 oauth_bp = Blueprint(
     'oauth_bp',
@@ -35,7 +36,29 @@ def oauth2_callback(service):
         patient = smart.patient
         print(smart.patient_id)
         print(smart.server.auth.access_token)
+
+        user = UserModel.query.filter_by(patient_id=smart.patient_id).first()
+        if user:
+            pass
+        else:
+            user = UserModel(
+                given_name = patient.name[0].given[0],
+                family_name =  patient.name[0].family,
+                date_of_birth = date.fromisoformat(patient.birthDate.isostring),
+                identifier_system = None,
+                identifier_value = None,
+                oauth_server = service,
+                patient_id = smart.patient_id,
+                email = None,
+                password = uuid.uuid1().__str__() #to be deprecated in oauth version
+            )
+            db.session.add(user)
+            db.session.commit()
+
+        login_user(user)
+
         flash(f'Successfully authorized by {service.upper()}! Welcome, {smart.human_name(smart.patient.name[0])}!')
+
     else:
         flash(f'Authorization Failed! Please try again.')
 
