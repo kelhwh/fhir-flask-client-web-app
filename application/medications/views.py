9c5825b261ed256590196c88bcdf48fd369e899c
kelhwh <frank1571@gmail.com>
2021-09-21 16:34:45 +0800

    ready medications

--- application/medications/views.py
+++ application/medications/views.py
@@ -1,5 +1,4 @@
 from flask import render_template, Blueprint, redirect, url_for, request
-from flask_login import current_user, login_required
 from fhirclient.models import condition, practitioner
 from application.fhir.connect import smart
 from application.fhir.search import ResourceFinder
@@ -14,19 +13,23 @@ medications_bp = Blueprint(
 )
 
 @medications_bp.route('/list')
-@login_required
 def list():
     MedicationRequestFinder = ResourceFinder('MedicationRequest', smart.server)
 
-    resouce_list = MedicationRequestFinder.find_by_patient(current_user.patient_id).resource_list()
+    result = MedicationRequestFinder.find_by_patient(smart.patient_id)
+    resource_list = [item for item in result.resource_list() if item.resource_type==MedicationRequestFinder.resource_type]
 
     medications = []
-    for m in resouce_list:
+    for m in resource_list:
         m = m.as_json()
 
         dict = {}
-        dict['medication_name'] = m.get('medicationCodeableConcept', {}).get('text')
-        dict['date'] = datetime.fromisoformat(m.get('authoredOn')).date().__str__()
+
+        # This seems to be Epic specific format, normally it would be:
+        # dict['medication_name'] = m.get('medicationCodeableConcept', {}).get('text')
+        dict['medication_name'] = m.get('medicationReference', {}).get('display')
+
+        dict['date'] = m.get('authoredOn')
         if m.get('requester', {}).get('agent', {}).get('reference') is not None:
             resource = practitioner.Practitioner.read_from(m.get('requester', {}).get('agent').get('reference'), smart.server)
             dict['practitioner_name'] = smart.human_name(resource.name[0])
@@ -38,11 +41,10 @@ def list():
         medications.append(dict)
         medications = [m for m in sorted(medications, key=lambda item: item['date'], reverse=True)]
 
-    return render_template('list.html', medications=medications)
+    return render_template('list.html', medications=medications, smart=smart)
 
 
 @medications_bp.route('/grid', methods=['GET'])
-@login_required
 def grid():
     search_date = request.args.get('search_date') or '2014-01-25'
     search_date = datetime.fromisoformat(search_date).date()
@@ -51,15 +53,18 @@ def grid():
     date_range = pd.date_range(start=start,end=end)
 
     search_params = [
-        ('patient', str(current_user.patient_id)),
+        ('patient', str(smart.patient_id)),
         ('effective-time', 'ge' + start.__str__()),
         ('effective-time', 'le' + end.__str__())
     ]
 
     MedicationAdministrationFinder = ResourceFinder.build('MedicationAdministration', smart.server)
-    resouce_list = MedicationAdministrationFinder.find(search_params=search_params).resource_list()
+    result = MedicationAdministrationFinder.find(search_params=search_params)
+
+    resource_list = [item for item in result.resource_list() if item.resource_type==MedicationAdministrationFinder.resource_type]
+
     medications = []
-    for m in resouce_list:
+    for m in resource_list:
         m = m.as_json()
 
         dict = {}
@@ -69,4 +74,12 @@ def grid():
 
         medications.append(dict)
 
-    return render_template('grid.html', medications=medications, search_date=search_date, start=start, end=end, date_range=date_range)
+    return render_template(
+        'grid.html',
+        medications=medications,
+        search_date=search_date,
+        start=start,
+        end=end,
+        date_range=date_range,
+        smart=smart
+    )
