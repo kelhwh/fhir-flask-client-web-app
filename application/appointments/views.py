c82399ea24df3221ec2a2eb2f976d1d0acd0659d
kelhwh <frank1571@gmail.com>
2021-09-21 16:34:32 +0800

    ready appointments

--- application/appointments/views.py
+++ application/appointments/views.py
@@ -1,5 +1,4 @@
 from flask import render_template, Blueprint, request
-from flask_login import current_user, login_required
 from application.fhir.search import ResourceFinder
 from application.fhir.connect import smart
 from contextlib import suppress
@@ -14,15 +13,14 @@ appointments_bp = Blueprint(
 
 
 @appointments_bp.route('/list', methods=['GET'])
-@login_required
 def list():
     page = request.args.get('page', 1, type=int)
 
     OrganizationFinder = ResourceFinder.build('Organization', smart.server)
     EncounterFinder = ResourceFinder.build('Encounter', smart.server)
 
-    result = EncounterFinder.find_by_patient(current_user.patient_id, batch_size=ROWS_PER_PAGE, page=page)
-    appointments_list = result.resource_list()
+    result = EncounterFinder.find_by_patient(smart.patient_id, batch_size=ROWS_PER_PAGE, page=page)
+    appointments_list = [item for item in result.resource_list() if item.resource_type==EncounterFinder.resource_type]
 
     max_page = ((result.total or 0) // ROWS_PER_PAGE) + 1
     appointments = []
@@ -52,5 +50,6 @@ def list():
         'appointments.html'
         , appointments=appointments
         , max_page=max_page
-        , current_page = page
+        , current_page=page
+        , smart=smart
     )
